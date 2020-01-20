5ad3999ed90f4f5d85721d1a59feb96701f9f5ee
Calvin Aquino <calvin.de.aquino@clio.com>
2020-01-20 15:35:40 -0800

    Initial project

---
+++ personal-planner-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  personal-planner-swiftui
+//
+//  Created by Calvin Aquino on 2020-01-20.
+//  Copyright © 2020 Aquino. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
